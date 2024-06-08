febb51c529914ff867e59b118f8ab4290100dc45
Thien Huong Duong <109571399+dxd1019@users.noreply.github.com>
2024-06-08 17:18:54 +0100

    Update programming language and coding theme

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -10,6 +10,7 @@ import 'codemirror/theme/3024-day.css';
 import 'codemirror/theme/3024-night.css';
 import 'codemirror/theme/abbott.css';
 import 'codemirror/theme/abcdef.css';
+import 'codemirror/theme/ambiance-mobile.css';
 import 'codemirror/theme/ambiance.css';
 import 'codemirror/theme/ayu-dark.css';
 import 'codemirror/theme/ayu-mirage.css';
@@ -58,6 +59,7 @@ import 'codemirror/theme/rubyblue.css';
 import 'codemirror/theme/seti.css';
 import 'codemirror/theme/shadowfox.css';
 import 'codemirror/theme/solarized.css';
+import 'codemirror/theme/ssms.css';
 import 'codemirror/theme/the-matrix.css';
 import 'codemirror/theme/tomorrow-night-bright.css';
 import 'codemirror/theme/tomorrow-night-eighties.css';
@@ -71,37 +73,133 @@ import 'codemirror/theme/yonce.css';
 import 'codemirror/theme/zenburn.css';
 
 // modes
+import 'codemirror/mode/apl/apl';
+import 'codemirror/mode/asciiarmor/asciiarmor';
+import 'codemirror/mode/asn.1/asn.1';
+import 'codemirror/mode/asterisk/asterisk';
+import 'codemirror/mode/brainfuck/brainfuck'
 import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/clojure/clojure';
+import 'codemirror/mode/cmake/cmake';
+import 'codemirror/mode/cobol/cobol';
+import 'codemirror/mode/coffeescript/coffeescript';
+import 'codemirror/mode/commonlisp/commonlisp';
+import 'codemirror/mode/crystal/crystal';
 import 'codemirror/mode/css/css';
+import 'codemirror/mode/cypher/cypher';
+import 'codemirror/mode/d/d';
 import 'codemirror/mode/dart/dart';
 import 'codemirror/mode/django/django';
 import 'codemirror/mode/dockerfile/dockerfile';
+import 'codemirror/mode/dtd/dtd';
+import 'codemirror/mode/dylan/dylan';
+import 'codemirror/mode/ebnf/ebnf';
+import 'codemirror/mode/ecl/ecl';
+import 'codemirror/mode/eiffel/eiffel';
+import 'codemirror/mode/erlang/erlang';
+import 'codemirror/mode/factor/factor';
+import 'codemirror/mode/fcl/fcl';
+import 'codemirror/mode/forth/forth';
+import 'codemirror/mode/fortran/fortran';
+import 'codemirror/mode/gas/gas';
+import 'codemirror/mode/gfm/gfm';
+import 'codemirror/mode/gherkin/gherkin';
 import 'codemirror/mode/go/go';
+import 'codemirror/mode/groovy/groovy';
+import 'codemirror/mode/haml/haml';
+import 'codemirror/mode/handlebars/handlebars';
+import 'codemirror/mode/haskell/haskell';
+import 'codemirror/mode/haskell-literate/haskell-literate';
+import 'codemirror/mode/haxe/haxe';
+import 'codemirror/mode/htmlembedded/htmlembedded';
 import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/http/http';
+import 'codemirror/mode/idl/idl';
 import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jinja2/jinja2';
 import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/julia/julia';
+import 'codemirror/mode/livescript/livescript';
+import 'codemirror/mode/lua/lua';
 import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/mathematica/mathematica';
+import 'codemirror/mode/mbox/mbox';
+import 'codemirror/mode/meta'
+import 'codemirror/mode/mirc/mirc';
+import 'codemirror/mode/mllike/mllike';
+import 'codemirror/mode/modelica/modelica';
+import 'codemirror/mode/mscgen/mscgen';
+import 'codemirror/mode/mumps/mumps';
+import 'codemirror/mode/nginx/nginx';
+import 'codemirror/mode/nsis/nsis';
+import 'codemirror/mode/ntriples/ntriples';
+import 'codemirror/mode/octave/octave';
+import 'codemirror/mode/oz/oz';
+import 'codemirror/mode/pascal/pascal';
+import 'codemirror/mode/pegjs/pegjs';
+import 'codemirror/mode/perl/perl';
 import 'codemirror/mode/php/php';
+import 'codemirror/mode/pig/pig';
+import 'codemirror/mode/powershell/powershell';
+import 'codemirror/mode/properties/properties';
+import 'codemirror/mode/protobuf/protobuf';
+import 'codemirror/mode/pug/pug';
+import 'codemirror/mode/puppet/puppet';
 import 'codemirror/mode/python/python';
+import 'codemirror/mode/q/q';
 import 'codemirror/mode/r/r';
-import 'codemirror/mode/rust/rust';
+import 'codemirror/mode/rpm/rpm';
+import 'codemirror/mode/rst/rst';
 import 'codemirror/mode/ruby/ruby';
+import 'codemirror/mode/rust/rust';
+import 'codemirror/mode/sas/sas';
 import 'codemirror/mode/sass/sass';
+import 'codemirror/mode/scheme/scheme';
 import 'codemirror/mode/shell/shell';
+import 'codemirror/mode/sieve/sieve';
+import 'codemirror/mode/slim/slim';
+import 'codemirror/mode/smalltalk/smalltalk';
+import 'codemirror/mode/smarty/smarty';
+import 'codemirror/mode/solr/solr';
+import 'codemirror/mode/soy/soy';
+import 'codemirror/mode/sparql/sparql';
+import 'codemirror/mode/spreadsheet/spreadsheet';
 import 'codemirror/mode/sql/sql';
+import 'codemirror/mode/stex/stex';
+import 'codemirror/mode/stylus/stylus';
 import 'codemirror/mode/swift/swift';
+import 'codemirror/mode/tcl/tcl';
+import 'codemirror/mode/textile/textile';
+import 'codemirror/mode/tiddlywiki/tiddlywiki';
+import 'codemirror/mode/tiki/tiki';
+import 'codemirror/mode/toml/toml';
+import 'codemirror/mode/tornado/tornado';
+import 'codemirror/mode/troff/troff';
+import 'codemirror/mode/ttcn/ttcn';
+import 'codemirror/mode/turtle/turtle';
+import 'codemirror/mode/twig/twig';
+import 'codemirror/mode/vb/vb';
+import 'codemirror/mode/vbscript/vbscript';
+import 'codemirror/mode/velocity/velocity';
+import 'codemirror/mode/verilog/verilog';
+import 'codemirror/mode/vhdl/vhdl';
+import 'codemirror/mode/vue/vue';
+import 'codemirror/mode/webidl/webidl';
 import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/xquery/xquery';
+import 'codemirror/mode/yacas/yacas';
 import 'codemirror/mode/yaml/yaml';
+import 'codemirror/mode/z80/z80';
 
-const Editor = ({clientControllerRef, roomsDetail, selectedRoomDetail, selectedRoomContent, userName}) => {
+const Editor = ({clientControllerRef, roomsDetail, selectedRoomDetail, selectedRoomContent, userName, lang, theme}) => {
     const cursorRef = useRef({line: 0, ch: 0})
     const codeMirrorRef = useRef(null)
 
     useEffect(() => {
         codeMirrorRef.current = CodeMirror.fromTextArea(
             document.getElementById('realtimeEditor'), {
-            mode: 'javascript',
-            theme: 'material',
+            mode: lang,
+            theme: theme,
             lineNumbers: true,
             readOnly: false,
         })
@@ -123,13 +221,24 @@ const Editor = ({clientControllerRef, roomsDetail, selectedRoomDetail, selectedR
             codeMirrorRef.current.setValue('Choose a room and start editing\nAlternatively, join or create a room to get started')
         }  
 
-    }, [clientControllerRef, selectedRoomDetail, userName])
+    }, [clientControllerRef, selectedRoomDetail, userName, lang, theme])
     
     useEffect(() => {
         codeMirrorRef.current.setValue(selectedRoomContent)
         codeMirrorRef.current.setCursor(cursorRef.current)
         
     }, [selectedRoomContent])
+    useEffect(() => {
+        if (codeMirrorRef.current) {
+            codeMirrorRef.current.setOption('mode', lang);
+        }
+    }, [lang]);
+
+    useEffect(() => {
+        if (codeMirrorRef.current) {
+            codeMirrorRef.current.setOption('theme', theme);
+        }
+    }, [theme]);
 
     return (
         <textarea id="realtimeEditor"></textarea>
--- frontend/src/pages/EditorPage.js
+++ frontend/src/pages/EditorPage.js
@@ -9,7 +9,7 @@ import toast from 'react-hot-toast';
 
 const EditorPage = ({clientControllerRef}) => {
     const [lang, setLang] = useState('');
-    const [them, setThem] = useState('default')
+    const [theme, setTheme] = useState('material')
     const [userName, setUserName] = useState('');
     const roomsDetail = useRef([]);
     const [selectedRoomDetail, setSelectedRoomDetail] = useState();
@@ -25,9 +25,12 @@ const EditorPage = ({clientControllerRef}) => {
     }, [location.state.userName, location.state.roomsDetail]);
 
 
-    const changeTheme = (newTheme) => {
-        document.documentElement.setAttribute('data-theme', newTheme);
-        setThem(newTheme);
+    const changeTheme = (e) => {
+        setTheme(e.target.value);
+    };
+
+    const changeLanguage = (e) => {
+        setLang(e.target.value);
     };
     
     const openRoom = (roomDetail) => {
@@ -145,6 +148,8 @@ const EditorPage = ({clientControllerRef}) => {
                     selectedRoomDetail={selectedRoomDetail}
                     selectedRoomContent={selectedRoomContent}
                     userName={userName}
+                    lang={lang}
+                    theme={theme}
                 />
             </div>
 
@@ -158,39 +163,136 @@ const EditorPage = ({clientControllerRef}) => {
 
                     <label className="languageSelector">
                         Language
-                        <select value={lang} onChange={(e) => {setLang(e.target.value);}}>
+                        <select onChange={changeLanguage} value={lang}>
+                            <option value="apl">APL</option>
+                            <option value="asciiarmor">AsciiArmor</option>
+                            <option value="asn.1">ASN.1</option>
+                            <option value="asterisk">Asterisk</option>
+                            <option value="brainfuck">Brainfuck</option>
                             <option value="clike">C / C++ / C# / Java</option>
+                            <option value="clojure">Clojure</option>
+                            <option value="cmake">CMake</option>
+                            <option value="cobol">COBOL</option>
+                            <option value="coffeescript">CoffeeScript</option>
+                            <option value="commonlisp">Common Lisp</option>
+                            <option value="crystal">Crystal</option>
                             <option value="css">CSS</option>
+                            <option value="cypher">Cypher</option>
+                            <option value="d">D</option>
                             <option value="dart">Dart</option>
                             <option value="django">Django</option>
                             <option value="dockerfile">Dockerfile</option>
+                            <option value="dtd">DTD</option>
+                            <option value="dylan">Dylan</option>
+                            <option value="ebnf">Extended Backus–Naur form</option>
+                            <option value="ecl">ECL</option>
+                            <option value="eiffel">Eiffel</option>
+                            <option value="erlang">Erlang</option>
+                            <option value="factor">Factor</option>
+                            <option value="fcl">FCL</option>
+                            <option value="forth">Forth</option>
+                            <option value="fortran">Fortran</option>
+                            <option value="gas">GAS</option>
+                            <option value="gfm">GitHub Flavored Markdown</option>
+                            <option value="gherkin">Gherkin</option>
                             <option value="go">Go</option>
+                            <option value="groovy">Groovy</option>
+                            <option value="haml">HAML</option>
+                            <option value="handlebars">Handlebars</option>
+                            <option value="haskell">Haskell</option>
+                            <option value="haskell-literate">Haskell-literate</option>
+                            <option value="haxe">Haxe</option>
+                            <option value="htmlembedded">HTML embedded</option>
                             <option value="htmlmixed">HTML-mixed</option>
+                            <option value="http">HTTP</option>
+                            <option value="idl">IDL</option>
                             <option value="javascript">JavaScript</option>
+                            <option value="jinja2">Jinja2</option>
                             <option value="jsx">JSX</option>
+                            <option value="julia">Julia</option>
+                            <option value="livescript">LiveScript</option>
+                            <option value="lua">Lua</option>
+                            <option value="mathematica">Mathematica</option>
                             <option value="markdown">Markdown</option>
+                            <option value="mbox">mbox</option>
+                            <option value="meta">Meta</option>
+                            <option value="mirc">mIRC</option>
+                            <option value="mllike">ML-like</option>
+                            <option value="modelica">Modelica</option>
+                            <option value="mscgen">MscGen</option>
+                            <option value="mumps">MUMPS</option>
+                            <option value="nginx">Nginx</option>
+                            <option value="nsis">NSIS</option>
+                            <option value="ntriples">NTriples</option>
+                            <option value="octave">Octave</option>
+                            <option value="oz">Oz</option>
+                            <option value="pascal">Pascal</option>
+                            <option value="pegjs">PEG.js</option>
+                            <option value="perl">Perl</option>
                             <option value="php">PHP</option>
+                            <option value="pig">Pig</option>
+                            <option value="powershell">PowerShell</option>
+                            <option value="properties">Properties</option>
+                            <option value="protobuf">Protocol Buffers</option>
+                            <option value="pug">Pug</option>
+                            <option value="puppet">Puppet</option>
                             <option value="python">Python</option>
+                            <option value="q">Q</option>
                             <option value="r">R</option>
+                            <option value="rpm">RPM</option>
+                            <option value="rst">reStructuredText</option>
+                            <option value="ruby">Ruby</option>
                             <option value="rust">Rust</option>
                             <option value="ruby">Ruby</option>
+                            <option value="sas">SAS</option>
                             <option value="sass">Sass</option>
+                            <option value="scheme">Scheme</option>
                             <option value="shell">Shell</option>
+                            <option value="sieve">Sieve</option>
+                            <option value="slim">Slim</option>
+                            <option value="smalltalk">Smalltalk</option>
+                            <option value="smarty">Smarty</option>
+                            <option value="solr">Solr</option>
                             <option value="sql">SQL</option>
+                            <option value="sparql">SPARQL</option>
+                            <option value="spreadsheet">Spreadsheet</option>
+                            <option value="stex">sTeX, LaTeX</option>
+                            <option value="stylus">Stylus</option>
                             <option value="swift">Swift</option>
+                            <option value="tcl">Tcl</option>
+                            <option value="textile">Textile</option>
+                            <option value="tiddlywiki">TiddlyWiki</option>
+                            <option value="tiki">Tiki wiki</option>
+                            <option value="toml">TOML</option>
+                            <option value="tornado">Tornado</option>
+                            <option value="troff">troff</option>
+                            <option value="ttcn">TTCN</option>
+                            <option value="turtle">Turtle</option>
+                            <option value="twig">Twig</option>
+                            <option value="vb">VB.NET</option>
+                            <option value="vbscript">VBScript</option>
+                            <option value="velocity">Velocity</option>
+                            <option value="verilog">Verilog</option>
+                            <option value="vhdl">VHDL</option>
+                            <option value="vue">Vue.js app</option>
+                            <option value="webidl">Web IDL</option>
                             <option value="xml">XML</option>
-                            <option value="yaml">yaml</option>
+                            <option value="xquery">XQuery</option>
+                            <option value="yacas">YACAS</option>
+                            <option value="yaml">YAML</option>
+                            <option value="z80">Z80</option>
                         </select>
                     </label>
 
                     <label className="themeSelector">
                         Theme
-                        <select value={them} onChange={(e) => changeTheme(e.target.value)} className="seLang">
+                        <select onChange={changeTheme} value={theme}>
                             <option value="default">default</option>
                             <option value="3024-day">3024-day</option>
                             <option value="3024-night">3024-night</option>
                             <option value="abbott">abbott</option>
                             <option value="abcdef">abcdef</option>
+                            <option value="ambiance-mobile">ambiance-mobile</option>
                             <option value="ambiance">ambiance</option>
                             <option value="ayu-dark">ayu-dark</option>
                             <option value="ayu-mirage">ayu-mirage</option>
@@ -238,6 +340,7 @@ const EditorPage = ({clientControllerRef}) => {
                             <option value="seti">seti</option>
                             <option value="shadowfox">shadowfox</option>
                             <option value="solarized">solarized</option>
+                            <option value="ssms">ssms</option>
                             <option value="the-matrix">the-matrix</option>
                             <option value="tomorrow-night-bright">tomorrow-night-bright</option>
                             <option value="tomorrow-night-eighties">tomorrow-night-eighties</option>
